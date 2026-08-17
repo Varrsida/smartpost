c675844bbb0008480d69bfc7d8dddb8b4f9a51aa
VARRSIDA LAXMINARAYANAN <varrsidabommi@gmail.com>
2026-08-17 15:27:11 +0530

    Prepare SmartPost for Render deployment

--- app.py
+++ app.py
@@ -1212,5 +1212,5 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html', current_theme='theme-landing'), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
